cac7986178f2b447f2d4165ce2b3e09d7e67d4cf
INOCcent-afk <michael828inoc@gmail.com>
2020-09-08 12:50:44 +0800

    Updates

---
+++ precache-manifest.f846f9eec1be026f78c1f5fe067d3a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b2c353169d3a6ad4ccb82792a660f0",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "0ebd63bce3e57344d337",
+    "url": "/first-react-app/static/css/main.42c997a0.chunk.css"
+  },
+  {
+    "revision": "4243468e1712c338e9c0",
+    "url": "/first-react-app/static/js/2.537834b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.537834b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebd63bce3e57344d337",
+    "url": "/first-react-app/static/js/main.d8be745b.chunk.js"
+  },
+  {
+    "revision": "6bd015cdd0228c321a7d",
+    "url": "/first-react-app/static/js/runtime-main.93f4e4a8.js"
+  },
+  {
+    "revision": "4a952357388ed81d26c3a6ee34c3f51a",
+    "url": "/first-react-app/static/media/0220.4a952357.jpg"
+  }
+]);
