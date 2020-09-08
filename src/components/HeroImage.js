f98945a7b7bd1c3a41e2f38031a244636375fff4
INOCcent-afk <michael828inoc@gmail.com>
2020-09-08 12:28:55 +0800

    Added React Project files

---
+++ src/components/HeroImage.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function HeroImage() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <div className="hero-content-title">
+          <h1>Creative Solutions</h1>
+        </div>
+        <div className="hero-content-content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos,
+            quasi?
+          </p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos,
+            quasi?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos,
+            quasi?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroImage;
---
+++ src/components/MainItems.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+function MainItems() {
+  return (
+    <div className="showCase">
+      <div className="showCase-items">
+        <div className="item">
+          <div className="item-title">
+            <h1>Consisting</h1>
+          </div>
+          <div className="item-content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              ut amet possimus, incidunt corporis cumque sequi natus illo ipsum,
+              perferendis optio. Reprehenderit exercitationem eum, molestias
+              culpa consequuntur modi repudiandae eos.
+            </p>
+          </div>
+        </div>
+        <div className="item">
+          <div className="item-title">
+            <h1>Consisting</h1>
+          </div>
+          <div className="item-content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              ut amet possimus, incidunt corporis cumque sequi natus illo ipsum,
+              perferendis optio. Reprehenderit exercitationem eum, molestias
+              culpa consequuntur modi repudiandae eos.
+            </p>
+          </div>
+        </div>
+
+        <div className="item">
+          <div className="item-title">
+            <h1>Consisting</h1>
+          </div>
+          <div className="item-content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              ut amet possimus, incidunt corporis cumque sequi natus illo ipsum,
+              perferendis optio. Reprehenderit exercitationem eum, molestias
+              culpa consequuntur modi repudiandae eos.
+            </p>
+          </div>
+        </div>
+        <div className="item">
+          <div className="item-title">
+            <h1>Consisting</h1>
+          </div>
+          <div className="item-content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              ut amet possimus, incidunt corporis cumque sequi natus illo ipsum,
+              perferendis optio. Reprehenderit exercitationem eum, molestias
+              culpa consequuntur modi repudiandae eos.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainItems;
